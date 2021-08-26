f99532c2262adbb9436a908bb69fc81be5b9fb55
foleycolin00 <foleycolin00@gmail.com>
2021-08-26 11:06:08 -0400

    hw2

--- src/py/hw2.py
+++ src/py/hw2.py
@@ -1,8 +1,91 @@
+'''Reads in a CSV file
+Parses it
+Prints errors for the bad lines
+:param file the file to read
+:return an array of arrays containing all of the valid rows'''
 def readCSV(file):
-  with open('dog_breeds.txt', 'r') as reader:
-    for line in reader.readlines():
-      print(line, end='')
+  array = []
+  with open(file, 'r') as reader:
+  
+    lines = reader.readlines()
+    i = 0
+    headers = None
+    
+    while i < len(lines):
+      line = lines[i]
+    
+      #clear the comments
+      line = line[:line.find('#')] + "\n"
+      
+      #remove all whitespace
+      line = line.replace(' ', '')
+      line = line.replace('\n', '')
+      
+      #dont print empty lines
+      if len(line) > 0:
+      
+        #break each line on comma, continue onto next line
+        if line[-1] == ',':
+          lines[i+1] = line + lines[i+1]
+        else:
+          
+          #Parse the commas
+          split = line.split(',')
+          
+          #Create the Header template
+          if (headers == None):
+            headers = split
+            
+            #Remove any ignored columns
+            result = []
+            for col in split:
+              if '?' not in col:
+                result.append(col)
+            array.append(result)
+            
+          else:
+            row = []
+            
+            #Check for errors
+            errors = False
+            
+            #Incorrect number of cells
+            if len(headers) != len(split):
+              print(f'Error on line {i}: incorrect number of cells')
+              
+            for j in range(len(headers)):
+            
+              #Should be number
+              if headers[j][0].isupper():
+                if not isNumber(split[j]):
+                  errors = True
+                  print(f'Error on line {i}: string where number should be')
+              
+              #Remove any ignored columns
+              if '?' not in headers[j]:
+                row.append(split[j])
+                
+            
+            if not errors:
+              array.append(split)
+      
+      i+=1
+  return array
 
-def testReadCSV():
+'''Returns if the string is a number (int/float)
+:param num: the string
+:return: if the string is a number'''
+def isNumber(num):
+  try:
+    return True
+  except ValueError:
+    try: 
+      return True
+    except ValueError:
+      return False
 
-readCSV("hw2.csv")
+array = readCSV("hw2.csv")
+for line in array:
+  for value in line:
+    print(value, end = ",")
+  print()
---
+++ src/py/test_hw2.py
@@ -0,0 +1,37 @@
+from hw2 import *
+
+'''Test function isNumber()'''
+def testIsNumber():
+  assert isNumber("123") == True
+  assert isNumber("123.") == True
+  assert isNumber("12.3") == True
+  assert isNumber("=.123") == True
+  assert isNumber(".123") == True
+  assert isNumber("-123") == True
+  assert isNumber("-123.") == True
+  assert isNumber("-12.3") == True
+  assert isNumber("-.123") == True
+  assert isNumber("-0.123") == True
+  
+  assert isNumber(".-123") == False
+  assert isNumber("1.2.3") == False
+  assert isNumber("thisisnotanumber") == False
+
+'''Test function readCSV'''
+def testReadCSV():
+  expected = [['Outlook','Temp','?Humidity','Windy','Wins+','Play-'],
+  ['sunny',85,85,10,20],
+  ['sunny',80,90,12,40],
+  ['overcast',83,86,40,40],
+  ['rainy',70,96,40,50],
+  ['rainy',68,80,50,30],
+  ['rainy',65,70,4,10],
+  ['overcast',64,65,30,60],
+  ['sunny',72,95,7,20],
+  ['sunny',69,70,70,70],
+  ['rainy',75,80,80,40],
+  ['sunny',75,70,30,50],
+  ['overcast',72,90,60,50],
+  ['overcast',81,75,30,60],
+  ['rainy',71,91,50,40]]
+  assert readCSV("hw2.csv") == expected

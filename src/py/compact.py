d294de20959e89f1be6757bc2ed19a22224c9a3d
Azeeza Eagal <89092830+eagal-ncsu@users.noreply.github.com>
2021-11-13 14:00:00 -0500

    option b

--- src/py/compact.py
+++ src/py/compact.py
@@ -9,7 +9,24 @@ Class that handles compact printing
 class Compact:
       
     def printCompact(trees):
+        n = 0
+        typHolder = 0
+        first = True
 
         for tree in trees: 
             for branch in tree: 
+                #get first typ
+                if first: 
+                    typHolder = branch.typ
+                    first = False
+
+                    
+                if branch.typ == typHolder: 
+                    print( " " + str(branch))
+                else: 
+                    print("  " + str(branch))
+                n+=1
+
                 
+            print()
+            n+=1  
